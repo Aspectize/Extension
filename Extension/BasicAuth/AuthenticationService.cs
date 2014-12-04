0ffaaafbd2b2b399b9c535ba36bb50271c41f81a
Nicolas Roux <nicolas.roux@aspectize.com>
2014-12-04 18:36:05 +0100

    initial commit

---
+++ Extension/BasicAuth/AuthenticationService.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using Aspectize.Core;
+
+namespace BasicAuth
+{
+    public interface IInscriptionService
+    {
+        bool SignUp(string userName, string pwd);
+    }
+
+    [Service(Name = "BasicAuthenticationService")]
+    public class BasicAuthenticationService : IAuthentication, IUserProfile, IPersistentAuthentication, IInscriptionService //, IInitializable, ISingleton
+    {
+        [ParameterAttribute]
+        string DataBaseService = null;
+
+        AspectizeUser IAuthentication.Authenticate(string userName, string secret, AuthenticationProtocol protocol, HashHelper.Algorithm algorithm, string challenge)
+        {
+            IDataManager dm = EntityManager.FromDataBaseService(DataBaseService);
+
+            List<User> users = dm.GetEntities<User>(new QueryCriteria(User.Fields.UserName, ComparisonOperator.Equal, userName.ToLower().Trim()));
+
+            if (users.Count > 0)
+            {
+                User user = users[0];
+
+                bool match = PasswordHasher.CheckResponse(user.Password, challenge, algorithm, secret);
+
+                if (match)
+                {
+                    Dictionary<string, object> dicoProperties = new Dictionary<string, object>();
+
+                    dicoProperties.Add("UserName", user.UserName);
+
+                    List<string> roles = new List<string>();
+
+                    roles.Add("Registered");
+
+                    user.DateLastLogin = DateTime.Now;
+
+                    dm.SaveTransactional();
+
+                    return AspectizeUser.GetAuthenticatedUser(user.Id.ToString(), null, roles.ToArray(), dicoProperties);
+                }
+            }
+
+            return AspectizeUser.GetUnAuthenticatedUser();
+        }
+
+        DataSet IUserProfile.GetUserProfile()
+        {
+            AspectizeUser aspectizeUser = ExecutingContext.CurrentUser;
+
+            if (aspectizeUser.IsAuthenticated)
+            {
+                Guid userId = new Guid(aspectizeUser.UserId);
+
+                IDataManager dm = EntityManager.FromDataBaseService(DataBaseService);
+
+                IEntityManager em = dm as IEntityManager;
+
+                User user = dm.GetEntity<User>(userId);
+
+                if (user != null)
+                {
+                    CurrentUser currentUser = em.CreateInstance<CurrentUser>();
+
+                    em.AssociateInstance<IsUser>(currentUser, user);
+
+                    em.Data.AcceptChanges();
+
+                    return dm.Data;
+                }
+            }
+
+            return null;
+        }
+
+        bool IPersistentAuthentication.ValidateUser(AspectizeUser user)
+        {
+            IDataManager dm = EntityManager.FromDataBaseService(DataBaseService);
+
+            User appliUser = dm.GetEntity<User>(new Guid(user.UserId));
+
+            if (appliUser != null)
+            {
+                user["UserName"] = appliUser.UserName;
+
+                appliUser.DateLastLogin = DateTime.Now;
+
+                dm.SaveTransactional();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        bool IInscriptionService.SignUp(string userName, string pwd)
+        {
+            IDataManager dm = EntityManager.FromDataBaseService(DataBaseService);
+
+            IEntityManager em = dm as IEntityManager;
+
+            List<User> users = dm.GetEntities<User>(new QueryCriteria(User.Fields.UserName, ComparisonOperator.Equal, userName.ToLower().Trim()));
+
+            User user;
+
+            if (users.Count == 0)
+            {
+                user = em.CreateInstance<User>();
+
+                user.UserName = userName;
+                user.Password = pwd;
+
+                dm.SaveTransactional();
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+}
---
+++ Extension/BasicAuth/BasicAuth.Entities.cs
@@ -0,0 +1,133 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.ComponentModel;
+
+using Aspectize.Core;
+
+[assembly:AspectizeDALAssemblyAttribute]
+
+namespace BasicAuth
+{
+	public static partial class SchemaNames
+	{
+		public static partial class Entities
+		{
+			public const string User = "User";
+			public const string CurrentUser = "CurrentUser";
+		}
+
+		public static partial class Relations
+		{
+			public const string IsUser = "IsUser";
+		}
+	}
+
+	[SchemaNamespace]
+	public class DomainProvider : INamespace
+	{
+		public string Name { get { return GetType().Namespace; } }
+		public static string DomainName { get { return new DomainProvider().Name; } }
+	}
+
+
+	[DataDefinition]
+	public class User : Entity, IDataWrapper
+	{
+		public static partial class Fields
+		{
+			public const string Id = "Id";
+			public const string UserName = "UserName";
+			public const string Password = "Password";
+			public const string DateLastLogin = "DateLastLogin";
+			public const string UserAgent = "UserAgent";
+		}
+
+		void IDataWrapper.InitData(DataRow data, string namePrefix)
+		{
+			base.InitData(data, null);
+		}
+
+		[Data(IsPrimaryKey=true)]
+		public Guid Id
+		{
+			get { return getValue<Guid>("Id"); }
+			set { setValue<Guid>("Id", value); }
+		}
+
+		[Data(Size = 100, IsNullable = true)]
+		public string UserName
+		{
+			get { return getValue<string>("UserName"); }
+			set { setValue<string>("UserName", value); }
+		}
+
+		[Data(Size = 50, IsNullable = true, ServerOnly = true)]
+[System.Xml.Serialization.XmlIgnore]
+		public string Password
+		{
+			get { return getValue<string>("Password"); }
+			set { setValue<string>("Password", value); }
+		}
+
+		[Data(IsNullable = true, ExcludeFromConcurrencyCheck = true)]
+		public DateTime? DateLastLogin
+		{
+			get { return getValue<DateTime?>("DateLastLogin"); }
+			set { setValue<DateTime?>("DateLastLogin", value); }
+		}
+
+		[Data(IsNullable = true, ServerOnly = true)]
+[System.Xml.Serialization.XmlIgnore]
+		public string UserAgent
+		{
+			get { return getValue<string>("UserAgent"); }
+			set { setValue<string>("UserAgent", value); }
+		}
+
+	}
+
+	[DataDefinition(MustPersist = false)]
+	public class CurrentUser : Entity, IDataWrapper
+	{
+		public static partial class Fields
+		{
+			public const string Id = "Id";
+		}
+
+		void IDataWrapper.InitData(DataRow data, string namePrefix)
+		{
+			base.InitData(data, null);
+		}
+
+		[Data(IsPrimaryKey=true)]
+		public Guid Id
+		{
+			get { return getValue<Guid>("Id"); }
+			set { setValue<Guid>("Id", value); }
+		}
+
+	}
+
+	[DataDefinition(MustPersist = false)]
+	public class IsUser : DataWrapper, IDataWrapper, IRelation
+	{
+		void IDataWrapper.InitData(DataRow data, string namePrefix)
+		{
+			base.InitData(data, null);
+		}
+
+		[RelationEnd(Type = typeof(CurrentUser), Role = typeof(CurrentUser), Multiplicity = Multiplicity.ZeroOrOne)]
+		public IEntity CurrentUser;
+
+		[RelationEnd(Type = typeof(User), Role = typeof(User), Multiplicity = Multiplicity.One)]
+		public IEntity User;
+
+	}
+
+}
+
+
+  
