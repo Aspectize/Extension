af77b59b31840b36f1022f9dcb3c217d8ec69129
FredyFadel <fredy@aspectize.com>
2015-10-29 16:30:56 +0100

    V1

--- Extension/AspectizePubNub/Aspectize/PubNub.js
+++ Extension/AspectizePubNub/Aspectize/PubNub.js
@@ -1,15 +1,51 @@
 ﻿/// <reference path="S:\Delivery\Aspectize.core\AspectizeIntellisenseLibrary.js" />
 
-function PubNubChannel (name) {
+function PubNubChannel(name, mergeData, handler) {
+
+    var executeCall = null;
+
+    function mergeAspectizeData(data) {
+
+        return Aspectize.Host.MergeRecievedData(data, mergeData);
+    }
+    
+    function both(m) {
+
+        if (!mergeAspectizeData(m)) {
+
+            executeCall(m);
+        }
+    }
+
     this.Name = name;
+
+    if (handler) {
+
+        var type = typeof (handler);
+        if (type === 'string') {
+
+            this.Receive = executeCall = function (m) {
+
+                Aspectize.Host.ExecuteCommand(handler, m, name);
+            };
+
+        } else if (type === 'function') {
+
+            this.Receive = executeCall = function (m) {
+
+                handler(m, name);
+            };
+
+        } else Aspectize.Throw(1, 'AspectizePubNub extension : bad handler type ' + type + " expected string or function. Check PubNubJS.InitializeReceive's second parameter !");
+
+        if (mergeData) this.Receive = both;
+
+    } else if (mergeData) this.Receive = mergeAspectizeData;
+        
 }
 
-PubNubChannel.prototype.Receive = function (m) {
 
-    alert(m);
-};
-
-function AspectizePubNub(name) {
+function AspectizePubNub(name, mergeData, handler) {
 
     this.Name = name;
     
@@ -26,11 +62,13 @@ function AspectizePubNub(name) {
 
     if(info.autoChannels) {
 
-        for(var n = 0; n < info.autoChannels.length; n++) {
-            
-            var channel = new PubNubChannel (info.autoChannels[n]);
+        if(mergeData || handler) {
 
-            this.pubnub.subscribe ({ channel: channel.Name, message : channel.Receive });            
+            for(var n = 0; n < info.autoChannels.length; n++) {
+            
+                var channel = new PubNubChannel(info.autoChannels[n], mergeData, handler);
+                this.pubnub.subscribe ({ channel: channel.Name, message : channel.Receive });            
+            }
         }
     }   
 }
@@ -39,11 +77,11 @@ var initPubNubService = (function () {
 
     var svcManager = {};
 
-    return function (name) {
+    return function (name, mergeData, handler) {
 
         if (!svcManager[name]) {
 
-            svcManager[name] = new AspectizePubNub(name);
+            svcManager[name] = new AspectizePubNub(name, mergeData, handler);
         }
 
         return svcManager[name];
@@ -55,6 +93,11 @@ Global.PubNub = {
    aasService:'PubNubJS',
    aasPublished:true,        
 
+   InitializeReceive: function (pubnubServiceName, mergeData, customHandlerOrCommand) {
+
+       initPubNubService(pubnubServiceName, mergeData, customHandlerOrCommand);
+   },
+
    SendChannelMessage: function (pubnubServiceName, channel, message) {
 
        var apn = initPubNubService(pubnubServiceName);
@@ -64,7 +107,6 @@ Global.PubNub = {
        obj.message = message;
        apn.pubnub.publish(obj);
    }
-
    
 };
 
